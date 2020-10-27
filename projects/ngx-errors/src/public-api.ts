a3c27c2f52daa56bf35c52182efca3b683b36502
ilDug <dognini.marco@gmail.com>
2020-10-27 13:43:35 +0100

    passaggio ad Angular 10

--- projects/ngx-errors/src/public-api.ts
+++ projects/ngx-errors/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-errors
  */
 
-export * from './lib/ngx-errors.module';
 export * from './lib/error.directive';
 export * from './lib/errors.directive';
+export * from './lib/ngx-errors.module';
